451a1d429c8530214dea56841460e01c9f876ae0
gdesiganax <afeexco223@yahoo.com>
2024-10-11 02:12:25 +0100

    Instad of saving, it now talking on the browser

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_file
 from gtts import gTTS
+from io import BytesIO
 import os
 
 app = Flask(__name__)
@@ -17,7 +18,11 @@ def speak():
 
     # Convert text to speech
     tts = gTTS(text=text, lang=language)
-    tts.save('static/speech.mp3')
+    audio = BytesIO()
+    tts.write_to_fp(audio)
+    audio.seek(0)
+    #tts = gTTS(text=text, lang=language)
+    #tts.save('static/speech.mp3')
 
     # Send the generated speech files
     return send_file('static/speech.mp3', as_attachment=True)
